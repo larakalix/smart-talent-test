5aa369ccec5dfc9f52f72f12e662be2cacc14a4c
Ivan Lara <ukikalix@gmail.com>
2025-03-08 19:03:33 -0500

    init commit

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
-  format: ["cjs", "esm"],
-  dts: true,
-  external: ["react"],
-  ...options,
+    entryPoints: ["src/index.tsx"],
+    format: ["cjs", "esm"],
+    dts: true,
+    external: ["react"],
+    ...options,
 }));
