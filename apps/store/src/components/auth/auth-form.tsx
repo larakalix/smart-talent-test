dbda92a1cb68ffa55db3975dc53d0bb9c82adfc8
larakalix <ukikalix@gmail.com>
2025-03-11 22:22:15 -0400

    ft/fake users added

--- apps/store/src/components/auth/auth-form.tsx
+++ apps/store/src/components/auth/auth-form.tsx
@@ -19,9 +19,36 @@ export const AuthForm = () => {
                 <span>Login as CLIENT</span>
                 <Button
                     className="block w-full rounded-sm bg-violet-400 p-4 text-sm font-medium transition hover:bg-violet-500"
-                    onClick={clientAuth}
+                    onClick={() => clientAuth("1")}
                 >
-                    Login as CLIENT
+                    Login as CLIENT #1
+                </Button>
+            </li>
+            <li>
+                <span>Login as CLIENT</span>
+                <Button
+                    className="block w-full rounded-sm bg-violet-400 p-4 text-sm font-medium transition hover:bg-violet-500"
+                    onClick={() => clientAuth("2")}
+                >
+                    Login as CLIENT #2
+                </Button>
+            </li>
+            <li>
+                <span>Login as CLIENT</span>
+                <Button
+                    className="block w-full rounded-sm bg-violet-400 p-4 text-sm font-medium transition hover:bg-violet-500"
+                    onClick={() => clientAuth("3")}
+                >
+                    Login as CLIENT #3
+                </Button>
+            </li>
+            <li>
+                <span>Login as CLIENT</span>
+                <Button
+                    className="block w-full rounded-sm bg-violet-400 p-4 text-sm font-medium transition hover:bg-violet-500"
+                    onClick={() => clientAuth("4")}
+                >
+                    Login as CLIENT #4
                 </Button>
             </li>
         </ul>
--- apps/store/src/components/auth/hooks/use-auth-form.ts
+++ apps/store/src/components/auth/hooks/use-auth-form.ts
@@ -1,9 +1,44 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../../stores/auth-store";
+import { User } from "../../../types/user";
+import { toast } from "sonner";
 
 export const useAuthForm = () => {
     const navigate = useNavigate();
     const { login } = useAuthStore((state) => state);
+
+    const fakeUsers = new Map<string, User>([
+        [
+            "1",
+            {
+                id: "1",
+                role: "user",
+                email: "user@testmail.com",
+                name: "User",
+                phone: "1234567890",
+            },
+        ],
+        [
+            "2",
+            {
+                id: "2",
+                role: "user",
+                email: "jdoe@testmail.com",
+                name: "John Doe",
+                phone: "1234567890",
+            },
+        ],
+        [
+            "3",
+            {
+                id: "3",
+                role: "user",
+                email: "lcanseco@testmail.com",
+                name: "Luis Canseco",
+                phone: "1234567890",
+            },
+        ],
+    ]);
 
     const adminAuth = () => {
         login({
@@ -17,13 +52,22 @@ export const useAuthForm = () => {
         navigate("/orders");
     };
 
-    const clientAuth = () => {
+    const clientAuth = (id: string) => {
+        const user = fakeUsers.get(id);
+
+        if (!user) {
+            toast.error("User not found");
+            return;
+        }
+
+        const { name, email, phone } = user;
+
         login({
-            id: "1",
+            id,
             role: "user",
-            email: "user@testmail.com",
-            name: "User",
-            phone: "1234567890",
+            email,
+            name,
+            phone,
         });
 
         navigate("/");
